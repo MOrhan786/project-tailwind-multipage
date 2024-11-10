80fea383605209ab5064ba48187c770c7b067c86
MOrhan786 <164301446+MOrhan786@users.noreply.github.com>
2024-11-10 23:46:43 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import Featuring from '../components/Featuring';
+import Hero from '../components/hero';
+
+
+export default function  Home() {
+  return (
+    <div> 
+      <Hero />
+      <Featuring />
+    
+      
+    </div>
+  )
+}
